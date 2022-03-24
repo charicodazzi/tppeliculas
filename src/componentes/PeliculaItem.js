2b087b48717df6c6897cc34d5a18eac82a2f24f1
charicodazzi <charicodazzi@hotmail.com>
2022-03-24 14:47:59 -0300

    home style

--- src/componentes/PeliculaItem.js
+++ src/componentes/PeliculaItem.js
@@ -4,13 +4,16 @@ import AddIcon from "@mui/icons-material/Add";
 
 const PeliculaItem = ({ titulo, imagen, link }) => {
   return (
-    <Box className="pelicula-item" sx={{ bgcolor: "black" }}>
+    <Box
+      className="pelicula-item"
+      sx={{ bgcolor: "black", borderColor: "#9899D0" }}
+    >
       <Avatar className="pelicula-item-imagen" src={imagen} sx={{ mt: 1 }} />
       <p>{titulo}</p>
       <a href={link}>
-        <span>
+        <Box sx={{ mt: 2 }}>
           <AddIcon color="secondary"></AddIcon>
-        </span>
+        </Box>
       </a>
     </Box>
   );
