ac70932244b648715779c0e212e1b084f947c066
charicodazzi <charicodazzi@hotmail.com>
2022-03-30 18:15:49 -0300

    Buscador

--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -1,7 +1,43 @@
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { APIkey } from "./FuncionesAuxiliares";
+import Tarjeta from "./Tarjeta";
+
 const Buscador = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams({ query: "" });
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${APIkey}&language=es&query=${searchParams.get(
+        "query"
+      )}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  }, [searchParams]);
+
+  const handleChange = (e) => {
+    setSearchParams({ query: e.target.value });
+  };
   return (
     <div>
-      <h2>Acá va el buscador</h2>
+      <h2>Search</h2>
+      <input
+        type="text"
+        onChange={handleChange}
+        value={searchParams.get("query")}
+      ></input>
+
+      {movies &&
+        movies.map((movie) => (
+          <Tarjeta
+            titulo={movie.title}
+            imagen={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+          />
+        ))}
     </div>
   );
 };
